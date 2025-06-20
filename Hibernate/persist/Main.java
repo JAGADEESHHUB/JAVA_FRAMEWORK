e54058ed6e57051228aec5866f7425e6d010b240
Jagadeesh K <157303337+JAGADEESHHUB@users.noreply.github.com>
2025-06-20 21:49:25 +0530

    Update Main.java

--- Hibernate/persist/Main.java
+++ Hibernate/persist/Main.java
@@ -2,17 +2,16 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         Employee e=new Employee();
-        e.setId(2);
-        e.setName("jagadeesh");
-        e.setPhoneNumber(1234);
+        e.setId(1);
+        e.setName("kalpana");
+        e.setPhoneNumber(5678);
 
         Configuration config =new Configuration();
         config.addAnnotatedClass(org.example.Employee.class);
@@ -22,7 +21,14 @@ public class Main {
 
         Session session = factory.openSession();
 
+        Transaction transaction = session.beginTransaction();
+
         session.persist(e);
+
+        transaction.commit();
+
+        factory.close();
+        session.close();
 
 
     }
